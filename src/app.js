aa40c2cfd791937b3ae1c1a908677681f925a979
saket-gupta99 <saketg54@gmail.com>
2024-12-24 14:39:20 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Saket", lastName: "Gupta" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data saved to DB");
+});
+app.delete("/user", (req, res) => {
+  res.send("deleted successfully");
+});
+app.patch("/user", (req, res) => {
+  res.send("updating some part of the resource");
+});
+//'use' matches all HTTP methods
 app.use("/test", (req, res) => {
   res.send("Hello from the test server!");
-});
-app.use("/hello", (req, res) => {
-  res.send("Hello from the Hello server!");
-});
-app.use((req, res) => {
-  res.send("Hello from the server!");
 });
 
 app.listen(3000, () => {
