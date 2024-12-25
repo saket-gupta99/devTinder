d7b48a91ffa7c19dcebf8b05ad49ad493ead4e87
saket-gupta99 <saketg54@gmail.com>
2024-12-25 15:28:27 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -14,20 +14,40 @@ connectionDB()
     console.log("Couldn't connect to the DB");
   });
 
-app.post("/signup", async (req, res) => {
-  //create a new instance of User model
-  const user = new User({
-    firstName: "Kunal",
-    lastName: "Ghorpade",
-    email: "kunal@gmail.com",
-    password: "kunal@123",
-    age: 25,
-  });
+//middleware to turn json to js object
+app.use(express.json());
 
+// app.post("/signup", async (req, res) => {
+//   //create a new instance of User model
+//   const user = new User(req.body);
+
+//   try {
+//     await user.save();
+//     res.send("User added successfully");
+//   } catch (err) {
+//     res.status(400).send("Error saving user data");
+//   }
+// });
+
+//get one user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
   try {
-    await user.save();
-    res.send("User added successfully");
+    const user = await User.findOne({ email: userEmail });
+    if (!user) res.status(401).send("User not found");
+    res.send(user);
   } catch (err) {
-    res.status(400).send("Error saving user data");
+    res.status(400).send(err.message);
+  }
+});
+
+//get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) res.status(401).send("User not found");
+    res.send(users);
+  } catch (err) {
+    res.status(400).send(err.message);
   }
 });
